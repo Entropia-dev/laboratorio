36cda751d01d3770e5687ee26dd74b3c61ee5ecf
Santiago Ortega <ortegabsn@gmail.com>
2019-08-31 17:17:53 -0300

    Notas actualizadas.

--- TP1LAB2C2019/main.cpp
+++ TP1LAB2C2019/main.cpp
@@ -10,14 +10,14 @@ using namespace std;
 #include "menues.h"
 
 
-/// Notas/Tareas:
-/// 1)Se necesita mejorar la validacion de fecha
-/// 2)a veces falla el seleccionar genero en la carga de pacientes
-/// 3)fseek(p, -sizeof reg , 1 ,p)
-/// 4)Falta comprobar que esten todas las validaciones de pacientes (ya funcionan todas las opciones del menu correctametne)
-/// 5)Averiguar la forma mas limpia de recorrer el archivo en modo lectura
-/// 6)Aniadir funcion que lea un solo registro ?
-/// 7)Darle forma al menu principal �cambiar colores?
+/// Notas/Tareas :
+/// 1)Se necesita mejorar la validacion de fecha.
+/// 2)a veces falla el seleccionar genero en la carga de pacientes.
+/// 3)fseek(p, -sizeof reg , 1 ,p).
+/// 4)Falta comprobar que esten todas las validaciones de pacientes (ya funcionan todas las opciones del menu correctametne).
+/// 5)Averiguar la forma mas limpia de recorrer el archivo en modo lectura.
+/// 6)Aniadir funcion que lea un solo registro ?.
+/// 7)Darle forma al menu principal �cambiar colores?.
 
 int main()
 {
