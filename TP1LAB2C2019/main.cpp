7e0f83c776fbe701a6ea983f45b533d9eb64c96e
Entropia-dev <54455913+Entropia-dev@users.noreply.github.com>
2019-09-19 19:08:26 -0300

    Notas para mañana

--- TP1LAB2C2019/main.cpp
+++ TP1LAB2C2019/main.cpp
@@ -17,16 +17,96 @@ using namespace std;
 #include "controles.h"
 #include "menues.h"
 
-
+/**
 
 /// Notas/Tareas :
 /// IMPORTANTES:
 /// PREGUNTAR SI EL FTELL(); NECESITA EL ARCHIVO ABIERTO PREVIAMENTE
 /// FUNCIONES CASI DUPLICADAS LISTAR CONTROL Y MOSTRAR CONTROL PREGUNTAR SI ES PREFERIBLE ELIMINAR ALGUNA
-/// REVISAR LOS WARNINGS
 
 /// EJEMPLO DE FSEEK : fseek(p, -sizeof reg , 1 ,p).
 
+
+FUNCIONES DE EJEMPLO PARA REALIZAR LOS BACKUPS
+
+[17:57, 19/9/2019] F: void restaurarInmuebles()
+{
+    Inmueble reg;
+
+    FILE  *pback,*p;
+    pback=fopen("bck/backupInmuebles.dat","rb");
+    if(pback==NULL)
+    {
+        cout<<"No existe archivo para poder realizar la restauracion de inmuebles"<<endl;
+        return ;
+    }
+    p=fopen("inmuebles.dat","wb");
+    if(p==NULL)
+    {
+        return ;
+    }
+    fclose(p);
+    p=fopen("inmuebles.dat","ab");
+    if(p==NULL)
+    {
+        return ;
+    }
+    while(fread(&reg,sizeof(Inmueble),1,pback)==1)
+    {
+
+        fwrite(&reg,sizeof(Inmueble),1,p);
+    }
+    cout<<"Se restauro el archivo inmuebles.dat con exito!"<<endl;
+    fclose(p);
+    fclose(pback);
+    return;
+}
+[17:57, 19/9/2019] F: void bckInmueble()
+{
+    Inmueble reg;
+    FILE  *pback,*p;
+    p=fopen("inmuebles.dat","rb");
+    if(p==NULL)
+    {
+        cout<<"El archivo inmuebles.dat no existe"<<endl;
+        return ;
+    }
+    pback=fopen("bck/backupInmuebles.dat","wb");
+    if(pback==NULL)
+    {
+        return ;
+    }
+    fclose(pback);
+    pback=fopen("bck/backupInmuebles.dat","ab");
+    if(pback==NULL)
+    {
+        return ;
+    }
+    while(fread(&reg,sizeof(Inmueble),1,p)==1)
+    {
+
+        fwrite(&reg,sizeof(Inmueble),1,pback);
+    }
+    cout<<"Se realizo backup del archivo inmuebles.dat con exito!"<<endl;
+    fclose(p);
+    fclose(pback);
+    return;
+}
+
+
+
+carga de matriz dinamica ?
+
+Float (*matriz)[12];
+Matriz=(float (*) [12]) malloc(cant*12*sizeof (float))
+free(matriz);
+
+
+PREGUNTAR DENUEVO EL FSEEK DE SOBREESCRIBIR PACIENTE
+
+
+
+**/
 void inicializar_archivos()
 {
     FILE *p;
